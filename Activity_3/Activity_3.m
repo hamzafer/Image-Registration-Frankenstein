0e70bb134995f0e893e1cfbf4fbcf7d5d057408b
Hamza Zafar <hamzafer3@gmail.com>
2024-05-02 10:59:42 +0200

    Activity 3 new changes

--- Activity_3/Activity_3.m
+++ Activity_3/Activity_3.m
@@ -2,7 +2,8 @@
 clc
 clear all
 close all
-%% Step 1. Execute your Activity 2 code, and get the CP sets for the reference and sensed images (full set, no CP reduction)
+%% Transformation 1 - SIFT
+% Step 1. Execute your Activity 2 code, and get the CP sets for the reference and sensed images (full set, no CP reduction)
 % a) Read image and show it
 im_orig=imread('coin.png'); 
 rc = imref2d(size(im_orig));
@@ -14,7 +15,7 @@ rt = imref2d(size(im_t));
 CPs_Matlab_t_det= detectSIFTFeatures(im_t);
 [CPs_Matlab_t_feat,  CPs_Matlab_t_ext]  = extractFeatures(im_t,  CPs_Matlab_t_det);
 
-%% Step 2. Match the two sets using the matchFeatures function.
+% Step 2. Match the two sets using the matchFeatures function.
 
 % Note that the inputs are the feature values, not the CPs_extracted sets;
 % We keep all the CPs extracted, since this is beneficial for having more matched candidates
@@ -23,30 +24,151 @@ CPs_Matlab_t_det= detectSIFTFeatures(im_t);
 matched_set_ref = CPs_Matlab_orig_ext(indexPairs(:,1));
 matched_set_sen = CPs_Matlab_t_ext(indexPairs(:,2));
 
-%% Step 3. Show the matched candidates using the ShowmatchedFeatures function. Read the function documentation first
+% Step 3. Show the matched candidates using the ShowmatchedFeatures function. Read the function documentation first
 figure,ax=axes;
 showMatchedFeatures(im_orig, im_t, matched_set_ref, matched_set_sen, 'montage','Parent', ax);
 title(ax, 'Candidate point matches');
 legend(ax, 'Matched points Ref','Matched points Sensed');
 
 % We are not satisfied with the matching. Seems like two pairs are not correct and we have crossings
-%% Step 4. Matching process parameter tuning
+
+% Step 4. Matching process parameter tuning
 % Change any of these parameters: Matchthreshold, Maxratio, Metric, we use
 % maratio, which its defult is 0.6
-[indexPairs,matchmetric] = matchFeatures(CPs_Matlab_orig_feat, CPs_Matlab_t_feat,'Unique',true,'MaxRatio',0.4);
 
-matched_set_ref=CPs_Matlab_orig_ext(indexPairs(:,1));
-matched_set_sen=CPs_Matlab_t_ext(indexPairs(:,2));
+MaxRatios = [0.9, 0.7, 0.5, 0.4];
+for i = 1:length(MaxRatios)
+    [indexPairs, matchMetrics] = matchFeatures(CPs_Matlab_orig_feat, CPs_Matlab_t_feat, 'Unique', true, 'MaxRatio', MaxRatios(i));
 
-% Show your results and go on until you are satisfied with the results obtained
-figure, ax=axes;
-showMatchedFeatures(im_orig, im_t, matched_set_ref, matched_set_sen,'montage','Parent', ax);
-title(ax, 'Candidate point matches with refined parameters');
+    matched_set_ref = CPs_Matlab_orig_ext(indexPairs(:,1));
+    matched_set_sen = CPs_Matlab_t_ext(indexPairs(:,2));
+
+    % Show results
+    figure, ax = axes;
+    showMatchedFeatures(im_orig, im_t, matched_set_ref, matched_set_sen, 'montage', 'Parent', ax);
+    title(ax, ['Candidate Point Matches with MaxRatio = ', num2str(MaxRatios(i))]);
+    legend(ax, 'Matched points Ref', 'Matched points Sensed');
+
+    % Descriptive statistics of match scores
+    meanScore = mean(matchMetrics);
+    medianScore = median(matchMetrics);
+    stdScore = std(matchMetrics);
+    numMatches = length(matchMetrics);
+    
+    disp(['-----> MaxRatio = ', num2str(MaxRatios(i)), ':']);
+    disp(['Number of Matches = ', num2str(numMatches)]);
+    disp(['Mean Score = ', num2str(meanScore)]);
+    disp(['Median Score = ', num2str(medianScore)]);
+    disp(['Standard Deviation of Scores = ', num2str(stdScore)]);
+end
+
+%% Transformation 1 - BRISK
+CPs_Matlab_orig_det = detectBRISKFeatures(im_orig);
+[CPs_Matlab_orig_feat,  CPs_Matlab_orig_ext]  = extractFeatures(im_orig, CPs_Matlab_orig_det);
+tform = affine2d([1 0 0; -0.3 1 0; 0 0 1]);
+[im_t,Rtr] = imwarp(im_orig, tform);
+rt = imref2d(size(im_t));
+CPs_Matlab_t_det= detectBRISKFeatures(im_t);
+[CPs_Matlab_t_feat,  CPs_Matlab_t_ext]  = extractFeatures(im_t,  CPs_Matlab_t_det);
+
+% Step 2. Match the two sets using the matchFeatures function.
+
+% Note that the inputs are the feature values, not the CPs_extracted sets;
+% We keep all the CPs extracted, since this is beneficial for having more matched candidates
+[indexPairs, ~] = matchFeatures(CPs_Matlab_orig_feat, CPs_Matlab_t_feat,'Unique',true);
+% We extract the selected matched points from the CP sets in the reference and sensed images
+matched_set_ref = CPs_Matlab_orig_ext(indexPairs(:,1));
+matched_set_sen = CPs_Matlab_t_ext(indexPairs(:,2));
+
+% Step 3. Show the matched candidates using the ShowmatchedFeatures function. Read the function documentation first
+figure,ax=axes;
+showMatchedFeatures(im_orig, im_t, matched_set_ref, matched_set_sen, 'montage','Parent', ax);
+title(ax, 'Candidate point matches');
 legend(ax, 'Matched points Ref','Matched points Sensed');
 
+% We are not satisfied with the matching. Seems like two pairs are not correct and we have crossings
 
-%%
-%Repeat everything for the second transformation
+% Step 4. Matching process parameter tuning
+% Change any of these parameters: Matchthreshold, Maxratio, Metric, we use
+% maratio, which its defult is 0.6
+
+MaxRatios = [0.9, 0.7, 0.5, 0.4];
+for i = 1:length(MaxRatios)
+    [indexPairs, matchMetrics] = matchFeatures(CPs_Matlab_orig_feat, CPs_Matlab_t_feat, 'Unique', true, 'MaxRatio', MaxRatios(i));
+
+    matched_set_ref = CPs_Matlab_orig_ext(indexPairs(:,1));
+    matched_set_sen = CPs_Matlab_t_ext(indexPairs(:,2));
+
+    % Show results
+    figure, ax = axes;
+    showMatchedFeatures(im_orig, im_t, matched_set_ref, matched_set_sen, 'montage', 'Parent', ax);
+    title(ax, ['Candidate Point Matches with MaxRatio = ', num2str(MaxRatios(i))]);
+    legend(ax, 'Matched points Ref', 'Matched points Sensed');
+
+    % Descriptive statistics of match scores
+    meanScore = mean(matchMetrics);
+    medianScore = median(matchMetrics);
+    stdScore = std(matchMetrics);
+    numMatches = length(matchMetrics);
+    
+    disp(['-----> MaxRatio = ', num2str(MaxRatios(i)), ':']);
+    disp(['Number of Matches = ', num2str(numMatches)]);
+    disp(['Mean Score = ', num2str(meanScore)]);
+    disp(['Median Score = ', num2str(medianScore)]);
+    disp(['Standard Deviation of Scores = ', num2str(stdScore)]);
+end
+
+%% Second Transformation - SIFT
+im_orig=imread('coin.png'); 
+rc = imref2d(size(im_orig));
+CPs_Matlab_orig_det = detectSIFTFeatures(im_orig);
+[CPs_Matlab_orig_feat,  CPs_Matlab_orig_ext]  = extractFeatures(im_orig, CPs_Matlab_orig_det);
+T2 = ([cosd(10) sind(10) 0; -sind(10) cosd(10) 0; 10 10 1]);
+Tform_rigid = affine2d(T2);
+[im_rigid, Rri] = imwarp(im_orig, Tform_rigid);
+CPs_Matlab_t_det2= detectSIFTFeatures(im_rigid);
+[CPs_Matlab_t_feat2,  CPs_Matlab_t_ext2]  = extractFeatures(im_rigid,  CPs_Matlab_t_det2);
+
+[indexPairs, ~] = matchFeatures(CPs_Matlab_orig_feat, CPs_Matlab_t_feat2,'Unique',true);
+% We extract the selected matched points from the CP sets in the reference and sensed images
+matched_set_ref = CPs_Matlab_orig_ext(indexPairs(:,1));
+matched_set_sen = CPs_Matlab_t_ext2(indexPairs(:,2));
+
+figure,ax=axes;
+showMatchedFeatures(im_orig, im_rigid, matched_set_ref, matched_set_sen, 'montage','Parent', ax);
+title(ax, 'Candidate point matches');
+legend(ax, 'Matched points Ref','Matched points Sensed');
+
+% Fine tuning
+
+MaxRatios = [0.9, 0.7, 0.5, 0.4];
+for i = 1:length(MaxRatios)
+    [indexPairs, matchMetrics] = matchFeatures(CPs_Matlab_orig_feat, CPs_Matlab_t_feat2, 'Unique', true, 'MaxRatio', MaxRatios(i));
+
+    matched_set_ref = CPs_Matlab_orig_ext(indexPairs(:,1));
+    matched_set_sen = CPs_Matlab_t_ext2(indexPairs(:,2));
+
+    % Show results
+    figure, ax = axes;
+    showMatchedFeatures(im_orig, im_rigid, matched_set_ref, matched_set_sen, 'montage', 'Parent', ax);
+    title(ax, ['Candidate Point Matches with MaxRatio = ', num2str(MaxRatios(i))]);
+    legend(ax, 'Matched points Ref', 'Matched points Sensed');
+
+    % Descriptive statistics of match scores
+    meanScore = mean(matchMetrics);
+    medianScore = median(matchMetrics);
+    stdScore = std(matchMetrics);
+    numMatches = length(matchMetrics);
+    
+    disp(['-----> MaxRatio = ', num2str(MaxRatios(i)), ':']);
+    disp(['Number of Matches = ', num2str(numMatches)]);
+    disp(['Mean Score = ', num2str(meanScore)]);
+    disp(['Median Score = ', num2str(medianScore)]);
+    disp(['Standard Deviation of Scores = ', num2str(stdScore)]);
+end
+
+%% Second Transformation - BRISK
+
 im_orig=imread('coin.png'); 
 rc = imref2d(size(im_orig));
 CPs_Matlab_orig_det = detectBRISKFeatures(im_orig);
@@ -67,16 +189,30 @@ showMatchedFeatures(im_orig, im_rigid, matched_set_ref, matched_set_sen, 'montag
 title(ax, 'Candidate point matches');
 legend(ax, 'Matched points Ref','Matched points Sensed');
 
-%%
 % Fine tuning
 
-[indexPairs,~] = matchFeatures(CPs_Matlab_orig_feat, CPs_Matlab_t_feat2,'Unique',true,'MaxRatio',0.5);
+MaxRatios = [0.9, 0.7, 0.5, 0.4];
+for i = 1:length(MaxRatios)
+    [indexPairs, matchMetrics] = matchFeatures(CPs_Matlab_orig_feat, CPs_Matlab_t_feat2, 'Unique', true, 'MaxRatio', MaxRatios(i));
 
-matched_set_ref=CPs_Matlab_orig_ext(indexPairs(:,1));
-matched_set_sen=CPs_Matlab_t_ext2(indexPairs(:,2));
+    matched_set_ref = CPs_Matlab_orig_ext(indexPairs(:,1));
+    matched_set_sen = CPs_Matlab_t_ext2(indexPairs(:,2));
 
-% Show your results and go on until you are satisfied with the results obtained
-figure, ax=axes;
-showMatchedFeatures(im_orig, im_rigid, matched_set_ref, matched_set_sen,'montage','Parent', ax);
-title(ax, 'Candidate point matches with refined parameters');
-legend(ax, 'Matched points Ref','Matched points Sensed');
+    % Show results
+    figure, ax = axes;
+    showMatchedFeatures(im_orig, im_rigid, matched_set_ref, matched_set_sen, 'montage', 'Parent', ax);
+    title(ax, ['Candidate Point Matches with MaxRatio = ', num2str(MaxRatios(i))]);
+    legend(ax, 'Matched points Ref', 'Matched points Sensed');
+
+    % Descriptive statistics of match scores
+    meanScore = mean(matchMetrics);
+    medianScore = median(matchMetrics);
+    stdScore = std(matchMetrics);
+    numMatches = length(matchMetrics);
+    
+    disp(['-----> MaxRatio = ', num2str(MaxRatios(i)), ':']);
+    disp(['Number of Matches = ', num2str(numMatches)]);
+    disp(['Mean Score = ', num2str(meanScore)]);
+    disp(['Median Score = ', num2str(medianScore)]);
+    disp(['Standard Deviation of Scores = ', num2str(stdScore)]);
+end
